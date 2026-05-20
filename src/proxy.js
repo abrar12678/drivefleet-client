c6f051f229a2fbbfce563bbee262fe6fca5af377
abrar12678 <fh053112@gmail.com>
2026-05-20 20:23:38 +0600

    Added Proxy

---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { getSession } from "better-auth/api";
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("sign-in", request.url));
+  }
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+export const config = {
+  matcher: ["/my-bookings", "/my-added-cars", "/add-car"],
+};
